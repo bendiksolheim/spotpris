9b8abd143af256ed0c3e73fbf449d4ac7b416e35
Bendik Solheim <hello@bendik.dev>
2023-01-28 19:54:09 +0100

    Fix api URL error

--- Shared/PricesAPI.swift
+++ Shared/PricesAPI.swift
@@ -3,11 +3,10 @@ import Foundation
 private let apiDateFormat = DateFormatter(format: "yyyy-MM-dd")
 
 func getPrices(area: Area, date: Date, completion: @escaping (Result<Data, Error>) -> Void) {
-    let url = "https://stroempris-apii.fly.dev/price/\(area)/\(apiDateFormat.string(from: date))"
+    let url = "https://stroempris-api.fly.dev/price/\(area)/\(apiDateFormat.string(from: date))"
     let headers = [
         "api-release-version": Bundle.main.releaseVersionNumber ?? "unknown",
         "api-build-version": Bundle.main.buildVersionNumber ?? "unknown",
-        "api-key": apiKey
     ]
     
     getData(url: url, headers: headers, completion: completion)
@@ -32,8 +31,6 @@ struct HourAndPrice: Identifiable {
     
     var id: Date { hour }
 }
-
-let apiKey = "b1e5014f-277c-4d93-b9f3-e45b32579d99"
 
 extension DateFormatter {
     convenience init(format: String, locale: Locale = Locale.current) {
